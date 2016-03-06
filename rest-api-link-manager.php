3be4bab892a001017f03600a6a87d90afdd4af09
JPry <JPry@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-03-06 17:46:51 +0000

    Update rest-api-link-manager.php
    git-svn-id: https://plugins.svn.wordpress.org/rest-api-link-manager/trunk@1365190 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rest-api-link-manager.php
+++ rest-api-link-manager.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Plugin Name: REST API Link Manager
- * Plugin URI: https://github.com/PrysPlugins/rest-api-link-manager
+ * Plugin URI:  https://github.com/PrysPlugins/rest-api-link-manager
  * Description: Add endpoints for managaing links to the REST API.
- * Version: 1.0
- * Author: Jeremy Pry
- * Author URI: http://jeremypry.com/
- * License: GPL2
+ * Version:     1.0.0
+ * Author:      Jeremy Pry
+ * Author URI:  http://jeremypry.com/
+ * License:     GPL2
  */
 
 // Prevent direct access to this file
@@ -14,10 +14,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( "You can't do anything by accessing this file directly." );
 }
 
-// Try loading our class
-if ( class_exists( 'WP_REST_Controller' ) ) {
-	require_once( __DIR__ . '/includes/class-jpry-rest-link-controller.php' );
-	define( 'JPRY_REST_LINK_LOADED', true );
+// Hook our class to the REST API.
+add_action( 'rest_api_init', 'jpry_register_rest_link_routes' );
+/**
+ * Register our routes with the REST API.
+ * 
+ * @since 1.0.0
+ */
+function jpry_register_rest_link_routes() {
+	jpry_require_once_file( __DIR__ . '/includes/class-jpry-rest-link-controller.php' );
+	$link_controller = new JPry_REST_Link_Controller();
+	$link_controller->register_routes();
 }
 
 /**
@@ -25,6 +32,8 @@ if ( class_exists( 'WP_REST_Controller' ) ) {
  *
  * This is intended to be used inside class methods to ensure that the
  * included file doesn't have access to the class scope.
+ * 
+ * @since 1.0.0
  *
  * @param string $file The full path to the file.
  */
@@ -35,4 +44,3 @@ function jpry_require_once_file( $file ) {
 
 	require_once( $file );
 }
-
