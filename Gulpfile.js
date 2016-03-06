ce6cfaf39d63d9fb3ab83616af53b30acae41434
JPry <JPry@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-03-06 17:47:45 +0000

    Create package.json and Gulpfile
    git-svn-id: https://plugins.svn.wordpress.org/rest-api-link-manager/trunk@1365194 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Gulpfile.js
@@ -0,0 +1,50 @@
+// Load dependencies
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
+var sort = require('gulp-sort');
+var wpPot = require('gulp-wp-pot');
+
+// Set assets paths.
+var paths = {
+	php: ['./*.php', './**/*.php'],
+};
+
+
+/**
+ * Handle errors and alert the user.
+ */
+function handleErrors() {
+	var args = Array.prototype.slice.call(arguments);
+
+	notify.onError({
+		title  : 'Task Failed [<%= error.message %>',
+		message: 'See console.',
+		sound  : 'Sosumi' // See: https://github.com/mikaelbr/node-notifier#all-notification-options-with-their-defaults
+	}).apply(this, args);
+
+	gutil.beep(); // Beep 'sosumi' again
+
+	// Prevent the 'watch' task from stopping
+	this.emit('end');
+}
+
+/**
+ * Scan the theme and create a POT file.
+ *
+ * https://www.npmjs.com/package/gulp-wp-pot
+ */
+gulp.task('wp-pot', function () {
+	return gulp.src(paths.php)
+		.pipe(plumber({errorHandler: handleErrors}))
+		.pipe(sort())
+		.pipe(wpPot({
+			domain   : 'jpry-rest-api-link-manager',
+			destFile : 'jpry-rest-api-link-manager.pot',
+			package  : 'jpry-rest-api-link-manager',
+			bugReport: 'https://github.com/PrysPlugins/rest-api-link-manager/issues'
+		}))
+		.pipe(gulp.dest('languages/'));
+});
+
+gulp.task('default', ['wp-pot']);
